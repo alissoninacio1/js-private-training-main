c06a4a28629948d8e8c5e22edf9ee79f17bde777
Álisson Inácio <69402669+alissoninacio1@users.noreply.github.com>
2022-09-13 14:22:55 -0600

    Update base.js

--- RegExp/base.js
+++ RegExp/base.js
@@ -1,6 +1,6 @@
 const text = ` 
 John trouxw flores para a sua amada namorada em 10 de janeiro de 1970.
-Maria era o nome dela.
+Maria era o nome dela..
 .
 Foir um ano excente na vida de Jhon. Teve 5 filhos, todos adultos atualmente. maria, hoje sus esposa, ainda lhe prepara aquele soboroso pao de queijo nas tardes de domingo. Também né! Sendo boa mineira que é, nunca esquece seu famoso pao de queijo.
 Sempre escuto a Maria dizer:
